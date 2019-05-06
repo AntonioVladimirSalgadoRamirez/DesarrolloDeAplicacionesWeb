8b993bcfe3d59e5f8c4504087f8ad5408c4d4d25
AntonioVladimirSalgadoRamirez <47458232+AntonioVladimirSalgadoRamirez@users.noreply.github.com>
2019-05-06 16:22:12 -0500

    Add files via upload

---
+++ DesarrolloAplicacionesWeb/agenda/agenda.js
@@ -0,0 +1,25 @@
+function agregar(){
+if (document.frm.txtEtiqueta.value.length==0){
+alert("debes escribir algo");
+} else{
+var nuevoItem = document.frm.txtEtiqueta.value;
+var combo = document.getElementById("selColores");
+var option =document.createElement("option");
+combo.options.add(option,0);
+combo.options[0].value=nuevoItem;
+combo.options[0].innerText=nuevoItem;
+document.frm.txtEtiqueta.value =""; 
+    } 
+}
+function eliminar(){
+var combo= document.getElementById("selColores");
+combo.remove(document.getElementById("selColores").selectedIndex);
+}               
+function modificar(){
+if(document.frm.txtEtiqueta.value.length ==0){
+  alert("debes escribir algo");
+} else{
+  eliminar();
+  agregar();
+}
+}      
